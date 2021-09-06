5994baeb69e67fadbc64d41c471917d8b9551d6b
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-06 18:01:28 -0300

    implemented the function where bring the information about the files on download directory path

--- aulas/aula01/gdrive-webapi/src/fileHelper.js
+++ aulas/aula01/gdrive-webapi/src/fileHelper.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+
 export default class FileHelper {
   static async getFilesStatus(downloadsFolder) {
+    const currentFiles = await fs.promises.readdir(downloadsFolder);
+    const statuses = await Promise.all(currentFiles.map(file => fs.promises.stat(`${downloadsFolder}/${file}`)));
 
-  }
+    for (const fileIndex in currentFiles) {
+      const { birthtime, size } = statuses[fileIndex];
+      console.log({ birthtime, size });
+    }
+  };
 };
